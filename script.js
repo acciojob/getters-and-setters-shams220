b0191df35a519011516ba6b6665004276ae57d5a
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-29 11:24:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name:string,age:number){
+		getName(){
+return this.name;
+		}
+		setAge(age){
+			this.age = age
+		}
+		study(){
+			console.log(`${this.name} is studying`);
+		}
+		teach(){class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  getName() {
+    console.log( this.name);
+  }
+  setAge(age) {
+    this.age = age;
+    console.log(this.age)
+  }
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+
+console.log(`${this.name} is teaching`)
+		}
+	}
+}
 
 class Student extends Person {}
 
